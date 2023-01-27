b297869015a4c418077f2339db399eff7217d9db
Vladimir <vladimir@presisconsultores.com>
2023-01-27 20:05:58 -0300

    changes on front

--- client/src/components/index/index.jsx
+++ client/src/components/index/index.jsx
@@ -5,29 +5,52 @@ import { Link } from "react-router-dom";
 import './style.css'
 
 function Index(){
-    const [notas, setNotas] = useState([])
-    useEffect(()=>{
-        axios.get('http://127.0.0.1:5000/'
-        ).then(res=>{
-            console.log(res.data.notas);
-            setNotas(res.data.notas);
-        }).catch(err=>console.log(err));
-    }, [])
+    // const [notas, setNotas] = useState([])
+    // useEffect(()=>{
+    //     axios.get('http://127.0.0.1:5000/'
+    //     ).then(res=>{
+    //         console.log(res.data.notas);
+    //         setNotas(res.data.notas);
+    //     }).catch(err=>console.log(err));
+    // }, [])
+    const notas = [
+        {title:'Title', author:'John Doe', date:'22/01/2023'},
+        {title:'Title', author:'John Doe', date:'22/01/2023'},
+        {title:'Title', author:'John Doe', date:'22/01/2023'},
+        {title:'Title', author:'John Doe', date:'22/01/2023'},
+        {title:'Title', author:'John Doe', date:'22/01/2023'},
+        {title:'Title', author:'John Doe', date:'22/01/2023'},
+        {title:'Title', author:'John Doe', date:'22/01/2023'},
+        {title:'Title', author:'John Doe', date:'22/01/2023'},
+        {title:'Title', author:'John Doe', date:'22/01/2023'},
+        {title:'Title', author:'John Doe', date:'22/01/2023'},
+        {title:'Title', author:'John Doe', date:'22/01/2023'},
+        {title:'Title', author:'John Doe', date:'22/01/2023'}   
+    ];
+
     let key = 0;
     return(
-        <div className="conteiner">
-            <h1>Index</h1>
-            {notas.map(n=>{
-                key+=1
-                return(
-                    <Link to={'/'+n.title} className="note" key={key}>
-                    <h4>{n.title}</h4>
-                        <div className="foot">
-                            <p>Por: {n.author}</p>
-                            <p>{n.date}</p>
-                        </div>
-                    </Link>
-                )})}
+        <div>
+            <header>
+                <h1>Bienvenido a mi Blog!</h1>
+                <p>Este es un espacio dedicado a aprender Raect y Flask con MongoDB, busco repasar conceptos como gestión de sesiones por token, permosos y middlewares en un proyecto desarrollado con microservicios</p>
+            </header>
+            <div className="conteiner">
+                {notas.map(n=>{
+                    key+=1
+                    return(
+                        <Link to={'/'+n.title} className="note" key={key}>
+                            <img src={'/'+n.image_url?'image_placeholder.jpg':n.image_url} alt="No image aviable" />
+                            <h4>{n.title}</h4>
+                            <div className="foot">
+                                <p>Por: {n.author}</p>
+                                <p>{n.date}</p>
+                            </div>
+                        </Link>
+                    );
+                })}
+            </div>
+
         </div>
         
     );
